c02f58781a00b217822a7f17ad9c7e73fb67cbd8
Leonel Mena <alejandro888peres@gmail.com>
2025-11-23 08:00:42 -0600

    FIX:Solucionado el error de sintaxis y verificado el calculo de conversion

---
+++ conversor_moneda.py
@@ -0,0 +1,25 @@
+# Variables Mi primer codigo (jeje)
+costo_cordobas = 58776  #
+tasa_cambio = 36.6  # Tasa de cambio promedio, si no la sabes, verifica en Google.
+
+# Calculo
+costo_usd = costo_cordobas / tasa_cambio
+
+# Mostrar el resultado
+print(f"El costo de la HP Omen de C$ {costo_cordobas} es:")
+print(f"Aproximadamente USD ${costo_usd:.2f}")
+
+# Ahora el script debe ser capaz de tomar esa cifra y almacenarla en una variable o archivo.
+
+# Variables
+costo_cordobas = 58776  #
+tasa_cambio = 36.6  # Tasa de cambio promedio, si no la sabes, verifica en Google.
+
+# Calculo
+costo_usd = costo_cordobas / tasa_cambio
+
+# Mostrar el resultado
+print(f"El costo de la HP Omen de C$ {costo_cordobas} es:")
+print(f"Aproximadamente USD ${costo_usd:.2f}")
+
+# Ahora el script debe ser capaz de tomar esa cifra y almacenarla en una variable o archivo.
